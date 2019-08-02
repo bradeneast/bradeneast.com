2e809247d6b75c72d20efec47586db212d128c17
Braden East <bradeneastdesign@gmail.com>
2019-08-02 18:23:58 -0500

    Lots of changes and updates
    Blog builder refactored
    Styles refactored
    Copy rewritten

---
+++ blog/build.js
@@ -0,0 +1,160 @@
+////>>>>RUN ME IN NODE TO BUILD OUT STATIC POSTS
+
+console.time('>> BUILD COMPLETE');
+// DECLARATIONS
+///////////////////////////////////////////////////
+
+// NODE THINGS
+const path = require('path');
+const fs = require('fs-extra');
+
+// PATHS
+const blogDir = './';
+const postDir = './_published/';
+const templateName = '_posttemplate.html';
+const postTemplate = blogDir + templateName;
+
+// DEPENDENCIES
+const htmlparser = require('htmlparser2');
+const cheerio = require('cheerio');
+
+// HELPERS
+// Sorts array of objects by the value of the property you pass
+function dynamicSort(property) {
+    var sortOrder = 1;
+    if (property[0] === "-") {
+        sortOrder = -1;
+        property = property.substr(1);
+    }
+    return function (a, b) {
+        var result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
+        return result * sortOrder;
+    }
+}
+
+
+// STUFF HAPPENS BELOW THIS LINE
+/////////////////////////////////////////////////////////////////
+
+
+// CLEARS POSTS FROM BLOG DIRECTORY
+fs.readdirSync(blogDir).forEach(dir => {
+    if (fs.lstatSync(blogDir + dir).isDirectory() && path.basename(blogDir + dir).charAt(0) !== '_') {
+        fs.removeSync(blogDir + dir);
+    }
+})
+
+
+// CREATES JSON ARRAY OF BLOG POSTS
+let posts = [];
+fs.readdirSync(postDir).forEach(item => {
+    const postName = path.basename(item, '.json');
+    if (postName != templateName) {
+        const postContent = JSON.parse(fs.readFileSync(`${postDir + postName}.json`, 'utf8'));
+        posts.push(postContent);
+    }
+})
+posts.forEach(post => {
+    post.link = encodeURI(post.title).replace(/%20|#/g, '-').replace(/\(|\)/g, '').replace(/--/g, '-').toLowerCase();
+})
+
+// SORTS ARRAY OF BLOG POSTS BY DATE
+posts = posts.sort(dynamicSort('date')).reverse();
+
+
+// GENERATES NEW POST FROM TEMPLATE
+posts.forEach((post, index) => {
+
+    // Copies post from template
+    let postLocation = blogDir + post.link;
+    fs.mkdirSync(postLocation);
+    postLocation = postLocation + '/index.html';
+    fs.copyFileSync(postTemplate, postLocation);
+    console.log('new post created from template');
+
+    // Parses post page html
+    const dom = htmlparser.parseDOM(fs.readFileSync(postLocation), {
+        decodeEntities: true
+    });
+    const $ = cheerio.load(dom);
+
+    // Inserts json values based on classnames matching the key
+    Object.keys(post).forEach(key => {
+        const e = $(`.${key}`);
+        let value = post[key];
+        if (Array.isArray(value)) {
+            value = value.join(', ');
+        }
+        if (value && e) {
+            e.append(value);
+            if (key == 'body') {
+                let title = post.link.replace(/-/g, '%20');
+                title = title.charAt(0).toUpperCase() + title.slice(1);
+                const url = 'https://bradeneast.com/blog/' + post.link;
+                e.append(`<br />Thanks for reading! If you learned something useful, <a target="_blank" href="https://twitter.com/share?text=${title}%20by%20@bradenthehair%20-%20&url=${url}">share this article</a> with your followers. I appreciate it!`);
+                console.log('cta appended to post body');
+            }
+        }
+    })
+
+    // Adds next and previous links
+    const prevPost = posts[index - 1];
+    const prevElem = $('#previous-post');
+    const nextPost = posts[index + 1];
+    const nextElem = $('#next-post');
+    if (prevPost) {
+        prevElem.append(prevPost.title);
+        prevElem.attr('href', `/blog/${prevPost.link}`);
+    } else {
+        prevElem.attr('style', 'opacity: 0');
+    }
+    if (nextPost) {
+        nextElem.append(nextPost.title);
+        nextElem.attr('href', `/blog/${nextPost.link}`);
+    } else {
+        nextElem.attr('style', 'opacity: 0');
+    }
+
+    // Writes the final markup to the post page
+    fs.writeFileSync(postLocation, $.html());
+    console.log('> post written to file ' + postLocation.replace('./', '/blog/'));
+})
+
+
+// ADDS POSTS TO HOMEPAGE
+const blogFeedPages = ['../blog/index.html'];
+blogFeedPages.forEach(page => {
+
+    // Parses page containing blog feed
+    const dom = htmlparser.parseDOM(fs.readFileSync(page), {
+        decodeEntities: true
+    });
+    const $ = cheerio.load(dom);
+    const feedElem = $('#blogFeed');
+    feedElem.children().not('template').remove();
+
+    posts.forEach(post => {
+
+        // Clones partial post template
+        const newPost = feedElem.find('template').contents().clone();
+
+        // Inserts json values based on classnames matching the key
+        Object.keys(post).forEach(key => {
+            const e = newPost.find(`.${key}`);
+            let value = post[key];
+            if (e && key == 'body') {
+                value = value.substr(0, 180) + '...';
+                e.append(value);
+            } else if (e && key == 'link') {
+                e.attr('href', value);
+            } else if (e) {
+                e.append(value);
+            }
+        });
+        feedElem.append(newPost);
+    })
+
+    fs.writeFileSync(page, $.html());
+})
+
+console.timeEnd('>> BUILD COMPLETE');
---
+++ scripts/insertheader.js
@@ -0,0 +1,44 @@
+const url = 'https://bradeneast.com' + window.location.pathname;
+let pageTitle = url.split('/').reverse()[1].replace(/-/g, ' ').replace(/%20/g, ' ');
+insertHeader(url, pageTitle);
+
+function insertHeader(url, pageTitle) {
+    const siteTitle = 'Braden East';
+    pageTitle = pageTitle.charAt(0).toUpperCase() + pageTitle.slice(1);
+    if (pageTitle === '' || pageTitle == null || url.split('/').length <= 4) {
+        pageTitle = siteTitle + ' &nbsp;|&nbsp; ' + 'Visual design solutions for nonprofits';
+    } else {
+        pageTitle = siteTitle + ' &nbsp;|&nbsp; ' + pageTitle;
+    }
+    const tagline = 'Helping nonprofits take control of their identity through branding and design.';
+    const socialImage = 'https://bradeneast.com/images/me-cropped-square.jpg';
+    document.head.insertAdjacentHTML('beforeend', `
+
+        <!-- Start SEO Framework -->
+        <title>${pageTitle}</title>
+        <meta name="description" content="${tagline}" />
+        <meta property="og:image" content="${socialImage}" />
+        <meta property="og:image:width" content="1080" />
+        <meta property="og:image:height" content="1080" />
+        <meta property="og:locale" content="en_US" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="${pageTitle}" />
+        <meta property="og:description" content="${tagline}" />
+        <meta property="og:url" content="${url}" />
+        <meta property="og:site_name" content="Braden East" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@bradenthehair" />
+        <meta name="twitter:title" content="${pageTitle}" />
+        <meta name="twitter:description" content="${tagline}" />
+        <meta name="twitter:image" content="${socialImage}" />
+        <link rel="canonical" href="${url}" />
+        <!-- End SEO Framework -->
+
+        <style>body{opacity:0;}</style>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="shortcut icon" type="image/png" href="/images/favicon.png" />
+        <link rel="stylesheet" type="text/css" href="/main.css" />
+    `);
+}
