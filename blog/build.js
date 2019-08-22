4e0f11f52cdce434d3bc259127ffc933c82c4242
Braden at Work <bradeneastdesign@gmail.com>
2019-08-22 10:40:29 -0500

    feed served as xml again

--- blog/build.js
+++ blog/build.js
@@ -72,17 +72,17 @@ fs.readdirSync(blogDir).map(dir => {
 })
 
 // Creates RSS feed location and starts RSS text string
-const feed = blogDir + 'feed';
+const feed = blogDir + 'feed.xml';
 fs.createFileSync(feed);
 const today = new Date();
 let RSSFeed = `<?xml version="1.0" encoding="utf-8"?>
     <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
     <channel>
     <title>Blog of Braden East</title>
-    <link>https://www.bradeneast.com/blog/feed</link>
+    <link>https://www.bradeneast.com/blog/feed.xml</link>
     <description>This blog is for developers and designers ready to execute their ideas.</description>
     <lastBuildDate>${today.toUTCString()}</lastBuildDate>
-    <atom:link href="https://www.bradeneast.com/blog/feed" rel="self" type="application/rss+xml" />
+    <atom:link href="https://www.bradeneast.com/blog/feed.xml" rel="self" type="application/rss+xml" />
 `;
 
 
