604cff666110a2c7813b2675080abe081906c0dd
Braden East <bradeneastdesign@gmail.com>
2019-10-11 13:46:57 -0500

    clear history

--- .history/babelify_20191011120355.js
+++
@@ -1,7 +0,0 @@
-let x = 2;
-
-function add2(number) {
-    return number + 2;
-}
-
-console.log(add2(2));
--- .history/babelify_20191011120424.js
+++
@@ -1,7 +0,0 @@
-let x = 2;
-
-const add2 = (number) => {
-    return number + 2;
-}
-
-console.log(add2(2));
