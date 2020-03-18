3f716222b940c455433139dd0c3ba3f5c5956205
Braden East <bradeneastdesign@gmail.com>
2020-03-18 17:53:40 -0500

    update css deferred link checking

--- js/main/main.js
+++ js/main/main.js
@@ -1,6 +1,5 @@
 import toggleDarkMode from './_toggleDarkMode.js';
 import processAnchor from './_processAnchor.js';
-import loadCSS from './_loadCSS.js';
 import altFromSrc from './_altFromSrc.js';
 import addCodepenFallback from './_addCodepenFallback.js';
 import findActive from './_findActive.js';
@@ -10,7 +9,15 @@ import skipToContent from './_skipToContent.js';
 
 
 // LINK tags
-for (let link of document.getElementsByTagName('link')) loadCSS(link);
+for (let link of document.getElementsByTagName('link')) {
+
+    if (link.href.includes('.css') && link.getAttribute('defer')) {
+
+        link.rel = 'stylesheet';
+
+    }
+
+}
 
 
 // Codepens
@@ -52,6 +59,7 @@ for (let image of document.getElementsByTagName('img')) {
 
 }
 
+
 // NAV
 let nav = document.getElementById('nav');
 let navItems = nav.getElementsByTagName('a');
@@ -66,6 +74,7 @@ if (active) {
 // DARK MODE
 let darkModeToggle = document.getElementById('dark_mode_toggle');
 listen(darkModeToggle, 'click', toggleDarkMode);
+
 
 // SKIP LINK
 let skipLink = document.getElementById('skip_link');
