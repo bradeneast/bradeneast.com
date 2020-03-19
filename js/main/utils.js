dd53572322aa6524da4d990f1e1edbf7fdccd9a0
Braden East <bradeneastdesign@gmail.com>
2020-03-19 10:57:31 -0500

    fix event default in listen() helper function

--- js/main/utils.js
+++ js/main/utils.js
@@ -1,4 +1,4 @@
-export function listen(element, callback, { event = 'click' }) {
+export function listen(element, callback, event = 'click') {
     try { element.addEventListener(event, callback) }
     catch (e) { element.attachEvent(`on${event}`, callback) }
 }
