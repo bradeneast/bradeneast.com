b653c291e75b00ea2b673a84c0f53fa848186670
Braden East <bradeneastdesign@gmail.com>
2020-05-07 11:21:34 -0500

    switch from fetch to xhttp

--- js/search/search.js
+++ js/search/search.js
@@ -1,31 +1,95 @@
-import handleSearch from './_handleSearch.js';
+let xhttp = new XMLHttpRequest();
+let pages = [];
+let resultsElem = document.getElementById('search_results');
+let resultTemplate = document.getElementById('search_result_template');
+let searchInput = document.getElementById('search');
 
 
-export let sitemap = fetch('/sitemap.json').then(function (r) { return r.json() });
-export let resultsElem = document.getElementById('search_results');
-export let resultTemplate = document.getElementById('search_result_template');
+
+xhttp.onreadystatechange = function () {
+    if (this.readyState != 4 || this.status != 200) return;
+    pages = JSON.parse(xhttp.responseText);
+    searchInput.addEventListener('keyup', handleSearchInput);
+}
+xhttp.open("GET", "/sitemap.json", true);
+xhttp.send();
 
 
-if (!('fetch' in window)) {
-    let searchWrapper = document.getElementById('search_wrapper');
-    searchWrapper ? searchWrapper.style.display = 'none' : null;
+
+function get(object, query = '') {
+    let props = query.split('.');
+    let value = { ...object };
+    props.map(prop => value = value[prop] || '');
+    return value;
 }
 
 
-let searchInput = document.getElementById('search');
+function search(value, inArray, matchProps) {
 
-searchInput.addEventListener('keyup', e => {
+    if (!value.length) return;
+    let searchValue = new RegExp(value, 'gi');
+    let results = [];
 
-    handleSearch(e.target.value);
-    history.replaceState(null, null, `${location.pathname}?${e.target.value}`);
+    for (let obj of inArray) {
 
-})
+        matchProps = matchProps.length ? matchProps : Object.keys(obj);
 
-if (location.search) {
+        for (let prop of matchProps) {
+            let value = get(obj, prop);
+            if (!searchValue.test(value)) continue;
+            if (results.find(r => r.content == obj)) continue;
+            results.push({
+                content: obj,
+                matchedBy: value.match(searchValue)
+            })
+        }
+    }
 
-    let search = location.search.replace('?', '');
+    return results;
+}
 
-    searchInput.value = search;
-    handleSearch(search);
 
+function handleSearchInput(event) {
+
+    let inputValue = event.target.value;
+    let matchProps = ['name', 'excerpt'];
+    let results = search(inputValue, pages, matchProps);
+
+    if (!results) return;
+    resultsElem.querySelectorAll('.result').forEach(r => r.remove());
+
+    // populate result template with new info for each page
+    for (let { content, matchedBy } of results) {
+
+        let template = resultTemplate.cloneNode(true);
+        let temp = template.innerHTML;
+        let dynamic = new RegExp('result\..+?(?=[\W< `"\'])', 'gi');
+        let matches = temp.match(dynamic) || [];
+
+        for (let match of matches) {
+
+            let query = match.replace('result.', '');
+            let value = get(content, query);
+
+            if (matchProps.includes(query)) {
+                let reMatch = new RegExp(matchedBy[0], 'gi');
+                value = value.replace(reMatch, `<mark>${matchedBy[0]}</mark>`);
+                temp = temp.replace(match, value);
+                continue;
+            }
+
+            temp = temp.replace(match, value);
+        }
+
+        template.removeAttribute('id');
+        template.classList.add('result');
+        template.innerHTML = temp;
+        resultsElem.appendChild(template);
+    }
+
+    history.replaceState(
+        null,
+        null,
+        `${location.pathname}?${event.target.value}`
+    );
 }
