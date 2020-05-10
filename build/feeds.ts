86c602186ab29371c659f7f88977978f6cf0e374
Braden East <bradeneastdesign@gmail.com>
2020-05-09 21:41:51 -0500

    fix duplicate categories bug

--- build/feeds.ts
+++ build/feeds.ts
@@ -5,9 +5,9 @@ import { deepCopy, firstElementChild, walkAst, linkify } from './utils.ts';
 import { pages } from './build.ts';
 
 
-export default function processFeeds(page) {
+export default function processFeeds(thisPage) {
 
-    let ast = HTML.parse(page.content);
+    let ast = HTML.parse(thisPage.content);
 
     for (let elem of walkAst(ast)) {
 
@@ -21,12 +21,18 @@ export default function processFeeds(page) {
         let targetPages = [];
 
         if (/categories/i.test(feedFrom)) {
+
             feedFrom = feedFrom.split('/categories/').pop();
-            targetPages = pages.filter(
-                page => page.categories.names.some(
-                    c => linkify(c) == feedFrom
-                )
-            );
+
+            for (let page of pages) {
+                let categoryMatch = page.categories.names.some(c => linkify(c) == feedFrom);
+                let scopeMatch = page.scopes.some(s => {
+                    return s.target.substring(1) == thisPage.parentDir.split('/')[0];
+                });
+                if (categoryMatch && scopeMatch) {
+                    targetPages.push(page);
+                }
+            }
         } else {
             targetPages = pages.filter(
                 page => page.scopes.some(
@@ -54,5 +60,5 @@ export default function processFeeds(page) {
         break;
     }
 
-    page.content = HTML.stringify(ast);
+    thisPage.content = HTML.stringify(ast);
 }
