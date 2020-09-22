89a67e8b2a853fd902749f29cb6a9636d3ef186b
Braden East <bradeneastdesign@gmail.com>
2020-09-21 22:19:54 -0500

    update animation handling

--- src/_js/main.js
+++ src/_js/main.js
@@ -3,8 +3,15 @@ import { $, altFromSrc, togglePreference, $$, elem, random } from './utils';
 
 function init() {
 
-	// If this script doesn't run, the reduced-motion class is left on the body element
+
+	// If this doesn't run, the reduced-motion class is left on the body element
 	document.body.classList.remove('reduced-motion');
+
+
+	// If this doesn't run, paused animations are not unpaused
+	for (let pausedAnimation of $$('.paused'))
+		pausedAnimation.classList.remove('paused');
+
 
 	// Preference Toggles
 	for (let preference of ['dark_mode', 'muted']) {
@@ -13,19 +20,16 @@ function init() {
 			toggle.addEventListener('click', () => togglePreference(preference))
 	}
 
+
 	// Pause animations outside the viewport
 	let animationHandler = new IntersectionObserver(entries =>
-		entries.map(entry =>
-			entry.target.classList
-				.toggle('paused', !entry.isIntersecting)
-		),
-		{
-			threshold: .63
-		}
+		entries.map(entry => entry.target.classList.toggle('paused', !entry.isIntersecting)),
+		{ threshold: .63 }
 	);
 	for (let animation of $$('.animation')) {
 		animationHandler.observe(animation);
 	}
+
 
 	// Character-split animations
 	let splits = document.querySelectorAll('.split');
@@ -33,9 +37,7 @@ function init() {
 		let chars = split.textContent
 			.split('')
 			.map((char, i) =>
-				`<span class="character" style="--index:${i}">
-							${char == ' ' ? '&nbsp;' : char}
-						</span>`
+				`<span class="character" style="--index:${i}">${char == ' ' ? '&nbsp;' : char}</span>`
 			);
 		split.innerHTML = chars.join('');
 	}
@@ -59,24 +61,25 @@ function init() {
 	}
 
 
-	// Embedded Projects
-	for (let project of $$('.embedded_project')) {
-
-		let button = $('button', project);
-		let iframe = $('iframe', project);
-
-		button.addEventListener('click', () => {
-			iframe.src = iframe.getAttribute('data-src');
-			project.classList.add('loaded');
-		})
-	}
-
 	// Listen on all links to the random page
 	for (let randomLink of $$('a[href*="/random"]'))
 		randomLink.addEventListener('click', event => {
 			event.preventDefault();
 			random(/\/blog\/.+/i);
 		});
+
+
+	// Embedded Projects
+	// for (let project of $$('.embedded_project')) {
+
+	// 	let button = $('button', project);
+	// 	let iframe = $('iframe', project);
+
+	// 	button.addEventListener('click', () => {
+	// 		iframe.src = iframe.getAttribute('data-src');
+	// 		project.classList.add('loaded');
+	// 	})
+	// }
 }
 
 init();
