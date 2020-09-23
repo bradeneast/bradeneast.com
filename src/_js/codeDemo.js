2605db8d3d648efffa472718bdcd1b558ff8044d
Braden East <beast@bgco.org>
2020-09-23 09:58:22 -0500

    add localstorage support for code demo builder

--- src/_js/codeDemo.js
+++ src/_js/codeDemo.js
@@ -1,14 +1,18 @@
-import { $ } from './parts/utils';
+import { $, ls } from './parts/utils';
 import Prism, { languages } from './libs/prism';
 
 let output = $('#output');
 let input = $('#input');
-let highlightButton = $('#highlightButton');
-let lang = 'javascript';
+let retrieved = ls('codeDemo');
+let lang = retrieved?.lang || 'javascript';
+let value = retrieved?.value || '';
 let shiftPressed = false;
+let waiter;
 
 
 let handleKeydown = event => {
+
+	clearTimeout(waiter);
 
 	let target = event.target;
 	let val = target.value;
@@ -29,34 +33,43 @@ let handleKeydown = event => {
 		if (shiftPressed) {
 			// Unindent current line by 1 tab
 			let precedingLines = preceding.split(/\n/);
-			let currentLine = precedingLines.pop();
-			target.value = precedingLines.join('\n') + currentLine.replace(/^\t/, '\n') + following;
+			let currentLine = precedingLines.pop().replace(/^\t/, '\n');
+			target.value = precedingLines.join('\n') + currentLine + following;
 			target.selectionStart = target.selectionEnd = start - 1;
+
 		} else {
 			// Indent current line by 1 tab
 			target.value = preceding + '\t' + following;
 			target.selectionStart = target.selectionEnd = start + 1;
 		}
-
 	}
+}
+
+
+let highlightInput = () => {
+	value = Prism.highlight(input.value, languages[lang], lang);
+	ls('codeDemo', { value: input.value, lang: lang });
+	output.innerHTML = value;
 }
 
 
 let handleKeyup = event => {
 	let keycode = event.keyCode || event.which;
+	waiter = setTimeout(highlightInput, 250);
 	if (keycode == 16)
 		shiftPressed = false;
 }
 
 
 addEventListener('input', event => {
-	if (event.target.name == 'lang')
+	if (event.target.name == 'lang') {
 		lang = event.target.id;
+		highlightInput();
+	}
 })
-
-
 input.addEventListener('keydown', handleKeydown);
 input.addEventListener('keyup', handleKeyup);
-highlightButton.addEventListener('click', () => {
-	output.innerHTML = Prism.highlight(input.value, languages[lang], lang);
-});
+
+input.value = value;
+$(`#${lang}`).checked = true;
+highlightInput();
