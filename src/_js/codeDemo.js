ea8d5e4336ced81b78c83094df1483be3c3238ea
Braden East <beast@bgco.org>
2020-09-23 09:08:39 -0500

    add tab support for code demo editor

--- src/_js/codeDemo.js
+++ src/_js/codeDemo.js
@@ -3,28 +3,60 @@ import Prism, { languages } from './libs/prism';
 
 let output = $('#output');
 let input = $('#input');
+let highlightButton = $('#highlightButton');
 let lang = 'javascript';
+let shiftPressed = false;
 
-$('#formatCode').addEventListener('click', () => {
-	output.innerHTML = Prism.highlight(input.value, languages[lang], lang);
-});
+
+let handleKeydown = event => {
+
+	let target = event.target;
+	let val = target.value;
+	let keycode = event.keyCode || event.which;
+
+	if (keycode == 16)
+		shiftPressed = true;
+
+	// Handle Tab Indentation
+	if (keycode == 9 && val.length) {
+		event.preventDefault();
+
+		let start = target.selectionStart;
+		let end = target.selectionEnd;
+		let preceding = val.slice(0, start);
+		let following = val.slice(end);
+
+		if (shiftPressed) {
+			// Unindent current line by 1 tab
+			let precedingLines = preceding.split(/\n/);
+			let currentLine = precedingLines.pop();
+			target.value = precedingLines.join('\n') + currentLine.replace(/^\t/, '\n') + following;
+			target.selectionStart = target.selectionEnd = start - 1;
+		} else {
+			// Indent current line by 1 tab
+			target.value = preceding + '\t' + following;
+			target.selectionStart = target.selectionEnd = start + 1;
+		}
+
+	}
+}
+
+
+let handleKeyup = event => {
+	let keycode = event.keyCode || event.which;
+	if (keycode == 16)
+		shiftPressed = false;
+}
+
 
 addEventListener('input', event => {
 	if (event.target.name == 'lang')
 		lang = event.target.id;
 })
 
-input.addEventListener('keydown', event => {
 
-	let target = event.target;
-	let val = target.value;
-	let keycode = event.keyCode || event.which;
-
-	if (keycode == 9) {
-		event.preventDefault();
-		let start = target.selectionStart;
-		let end = target.selectionEnd;
-		target.value = val.slice(0, start) + '\t' + val.slice(end);
-		target.selectionStart = target.selectionEnd = start + 1;
-	}
-})
+input.addEventListener('keydown', handleKeydown);
+input.addEventListener('keyup', handleKeyup);
+highlightButton.addEventListener('click', () => {
+	output.innerHTML = Prism.highlight(input.value, languages[lang], lang);
+});
