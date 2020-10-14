2d9a92751349e330739a46025a08ff4cc5becad8
Braden East <bradeneastdesign@gmail.com>
2020-10-14 10:12:08 -0500

    update ingrid

--- node_modules/ingrid-ssg/ingrid.config.js
+++ node_modules/ingrid-ssg/ingrid.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-	src: "./src",
-	dist: "./dist",
-	ignorePattern: /^_/,
-	global: {},
+	src: "./src", // The private directory where Ingrid looks for HTML and Markdown files
+	dist: "./dist", // The public directory where your site build is output
+	ignorePattern: /^_/, // By default, files and directories whose names begin with an underscore "_" are ignored
+	global: {}, // An object of values that can be interpolated anywhere in your site using double curly braces: {{example_value}}
 }
