b0b9431b3c18ae367fa67ecacf9623f96b183f5a
Braden East <bradeneastdesign@gmail.com>
2020-12-09 11:59:36 -0600

    remove intersection observer stuff

--- src/_js/main/parts/animations.js
+++ src/_js/main/parts/animations.js
@@ -17,31 +17,4 @@ export default function prepAnimations() {
 
 	for (let paused of $$('.paused'))
 		paused.classList.remove('.paused');
-
-
-	let animationClass = 'animate-in';
-	let observerTargets = $$(`.${animationClass}`);
-	let observer = new IntersectionObserver(handleIntersection, { threshold: 1 });
-
-	observerTargets.forEach(target => observer.observe(target));
-
-	// Pause animations outside the viewport
-	function handleIntersection(entries) {
-		entries.map(entry => {
-
-			let target = entry.target;
-			let isIntersecting = entry.isIntersecting;
-			let r = target.getBoundingClientRect();
-			let targetCenter = r.bottom - r.height / 2;
-			let viewportCenter = innerHeight / 2;
-			let scrollDirection = targetCenter > viewportCenter ? 1 : -1;
-			if (!isIntersecting) scrollDirection = 0;
-
-			target.style.setProperty('--animateFrom', scrollDirection);
-			target.classList.toggle('intersecting', isIntersecting);
-
-			if (isIntersecting)
-				setTimeout(() => target.classList.remove(animationClass), 1000);
-		})
-	}
 }
