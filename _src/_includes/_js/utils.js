2736efca29c37296ab2974f014dda26315018c05
Braden East <bradeneastdesign@gmail.com>
2021-09-23 08:47:33 -0500

    scripts update

--- _src/_includes/_js/utils.js
+++ _src/_includes/_js/utils.js
@@ -1,22 +1,10 @@
-/**A shorthand for `querySelector`*/
+/**A shorthand for `querySelector` */
 export let $ = (selector, context = document) => context.querySelector(selector);
-
-/**A shorthand for `querySelectorAll`*/
+/**A shorthand for `querySelectorAll` */
 export let $$ = (selector, context = document) => context.querySelectorAll(selector);
 
-/**Joins all arguments with a newline*/
-export let reformLines = (...args) => args.join('\n');
 
-/**Generates a random integer between 0 and the ceiling */
-export let randomInt = (max = 10) => Math.round(Math.random() * max);
-
-/**Replaces backslashes with forward slashes and removes trailing and double slashes from a path */
-export let normalize = (path) => path.replace(/\/$/, '').replace(/[/\\]+/g, '/');
-
-
-/**Refreshes the page */
-export let redirect = (target = location.href) => location = target;
-
+/**A shorthand for `document.createElement` */
 export let elem = (tagName, content = '') => {
 	let tag = document.createElement(tagName);
 	tag.innerHTML = content;
@@ -24,18 +12,13 @@ export let elem = (tagName, content = '') => {
 }
 
 
-/**A shorthand for `localStorage`
- * @param {string} key
- * @param {string} value
-*/
+/**A shorthand for `localStorage` */
 export let ls = (key, value) => value == undefined
 	? JSON.parse(localStorage.getItem(key))
 	: localStorage.setItem(key, JSON.stringify(value));
 
 
-/**Toggle a user preference saved in `localStorage` as a class on the `documentElement`
- * @param {string} className
-*/
+/**Toggle a user preference saved in `localStorage` as a class on the `documentElement` */
 export function togglePref(prefName) {
 
 	let modal = $('.modal');
@@ -45,50 +28,13 @@ export function togglePref(prefName) {
 	document.documentElement.classList.toggle(prefName, state);
 	ls(prefName, state);
 
-	if (modal) {
+	if (!modal) return;
+	modal.innerHTML = `${prefName.replace(/[-_]/g, ' ')} ${state ? 'on' : 'off'}`;
+	modal.classList.add('visible');
 
-		modal.innerHTML = `${prefName.replace(/[-_]/g, ' ')} ${state ? 'on' : 'off'}`;
-		modal.classList.add('visible');
-
-		let waiter;
-		waiter = setTimeout(() => {
-			clearTimeout(waiter);
-			modal.classList.remove('visible')
-		}, 2000);
-	}
-}
-
-
-/**Fetches /sitemap.xml and runs a callback on the document response
- * @param {function} callback
-*/
-export function getSitemap(callback) {
-	let xhttp = new XMLHttpRequest();
-	xhttp.onreadystatechange = function () {
-		if (this.status == 200 && xhttp.responseXML)
-			callback(xhttp.responseXML);
-	}
-	xhttp.responseType = 'document';
-	xhttp.open('GET', '/sitemap.xml', true);
-	xhttp.send();
-}
-
-
-/**Fetches the sitemap and picks a random loc to navigate to
- * @param {RegExp} matcher
- */
-export function getRandomPage(matcher) {
-	return getSitemap(sitemap => {
-		if (!sitemap)
-			window.location = '/random';
-
-		let locs = Array.from($$('loc', sitemap));
-		let hrefs = locs.map(loc => loc.textContent.trim());
-		let filtered = hrefs.filter(href => matcher.test(href));
-		let index = Math.round((filtered.length - 1) * Math.random());
-
-		if (filtered[index])
-			window.location = filtered[index];
-		else return false;
-	})
+	let waiter;
+	waiter = setTimeout(() => {
+		clearTimeout(waiter);
+		modal.classList.remove('visible')
+	}, 2000);
 }
--- _src/main.js
+++ _src/main.js
@@ -1,22 +1,66 @@
-import prepAnimations from './_includes/_js/animations';
-import prepCodeBlocks from './_includes/_js/codeBlocks';
-import startListeners from './_includes/_js/listeners';
-import prepMedia from './_includes/_js/media';
-import { $$ } from './_includes/_js/utils';
-import Schwifty from './_includes/_js/schwifty';
+import Schwifty from './_includes/_js/schwifty.js';
+import { $, $$, elem, togglePref } from './_includes/_js/utils.js';
+
 
 function init() {
 
+
+	// Set aria-current
 	$$(`a[href="${location.pathname}"]`)
 		.forEach(a => a.setAttribute('aria-current', 'page'));
 
-	prepAnimations();
-	prepMedia();
-	prepCodeBlocks();
-	startListeners();
+
+	// Remove paused class from animating elements
+	for (let paused of $$('.paused'))
+		paused.classList.remove('.paused');
+
+
+	// Add line numbers to code blocks
+	for (let code of $$('code[class*="language-"')) {
+
+		let pre = code.closest('pre');
+		let lineNumbers = elem('span');
+		let lineCount = code.innerHTML.split(/\n/).length;
+
+		lineNumbers.classList.add('line-numbers-rows');
+		lineNumbers.setAttribute('aria-hidden', true);
+		for (let i = 0; i < lineCount; i++)
+			lineNumbers.append(elem('span'));
+
+		pre.classList.add('line-numbers');
+		pre.append(lineNumbers);
+	}
+
+
+	// Add wrapper div to tables
+	for (let table of $$('table'))
+		table.outerHTML = `<div class="table-wrapper">${table.outerHTML}</div>`;
+
+
+	// Add codepen script for embedded Codepens
+	if ($('.codepen')) {
+		let script = elem('script');
+		script.src = 'https://static.codepen.io/assets/embed/ei.js';
+		script.async = true;
+		document.body.append(script);
+	}
+
+
+	// Listen on back-to-top button
+	$('#back_to_top').onclick = () => scrollTo(0, 0);
+
+
+	// Listen on preference toggles
+	for (let pref of window.__preferences) {
+		let toggle = $(`#${pref}_toggle`);
+		toggle.onclick = () => togglePref(pref);
+	}
+
 }
 
+
 init();
+
 
 new Schwifty({
 	onload: init,
